263c33013cdb895cd34677900b04e1875270e2f8
mathevargas <matheusvargask2@gmail.com>
2026-06-22 21:37:18 -0300

    revert: restaura versao base apos demonstracao

--- src/main/resources/db/migration/V3__create_categoria.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE categoria (
-                           id BIGSERIAL PRIMARY KEY,
-                           nome VARCHAR(100) NOT NULL,
-                           descricao TEXT,
-                           criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
